105df15c4c88596b38908a0bfcb323e1b8a4cc2e
Ben <benjamin.a.spencer@gmail.com>
2015-10-06 17:14:48 -0400

    complete revising string

---
+++ revising-string.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Define a variable named pizza that references this string: pizza is alright
+
+// Use the .replace() method to change alright to wonderful.
+
+// Use console.log() to print the results of the .replace() method to the terminal.
+
+var pizza = 'pizza is alright';
+pizza = pizza.replace('alright', 'wonderful');
+console.log(pizza)
+
+
+// var example = 'this example exists';
+// example = example.replace('exists', 'is awesome');
+// console.log(example);
+
+// Note that to change the value that the example variable references, we need
+// to use the equals sign again, this time with the example.replace() method to
+// the right of the equals sign.
