2b6dcd095ab04107f591f322dbc2c61bb043d3f6
Ben <benjamin.a.spencer@gmail.com>
2015-10-06 21:11:58 -0400

    complete accessing array values

---
+++ accessing-array-values.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// In that file, define array food :
+
+// var food = ['apple', 'pizza', 'pear'];
+
+// Use console.log() to print the second value of array to the terminal.
+
+var food = ['apple', 'pizza', 'pear'];
+
+console.log(food[1]);
